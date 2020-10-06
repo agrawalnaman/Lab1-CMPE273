88afc7b43c46ff584511bbc6eb6e2b182194fb36
Naman <naman.agrawal@sjsu.edu>
2020-10-06 12:06:25 -0700

    Map Added

--- Frontend/src/components/CustomerMain/CustomerMain.js
+++ Frontend/src/components/CustomerMain/CustomerMain.js
@@ -9,6 +9,7 @@ import Button from "react-bootstrap/esm/Button";
 import Form from 'react-bootstrap/Form';
 import Accordion from 'react-bootstrap/Accordion';
 import { Link } from "react-router-dom";
+import MapContainer from "../CustomerMain/MapRestaurants";
 
 
 //Define a Login Component
@@ -21,8 +22,8 @@ class CustomerMain extends Component {
         this.state = {
             searchterm: "",
             category: "",
-            restaurants:"",
-            restaurantnotfound:"",
+            restaurants: "",
+            restaurantnotfound: "",
         };
         this.searchHandler = this.searchHandler.bind(this);
         this.categoryChangeHandler = this.categoryChangeHandler.bind(this);
@@ -34,8 +35,8 @@ class CustomerMain extends Component {
         this.setState({
             searchterm: "",
             category: "",
-            restaurants:"",
-            restaurantnotfound:"",
+            restaurants: "",
+            restaurantnotfound: "",
 
         });
 
@@ -56,11 +57,13 @@ class CustomerMain extends Component {
         var headers = new Headers();
         //prevent page from refresh
         e.preventDefault();
-        const data = { params: { 
-            searchterm:this.state.searchterm,
-            category:this.state.category,
-        
-        } };
+        const data = {
+            params: {
+                searchterm: this.state.searchterm,
+                category: this.state.category,
+
+            }
+        };
         console.log("search term:", this.state.searchterm);
         //set the with credentials to true
         axios.defaults.withCredentials = true;
@@ -78,7 +81,7 @@ class CustomerMain extends Component {
 
                 } else {
                     this.setState({
-                        restaurants:"",
+                        restaurants: "",
                         restaurantnotfound: "No Restuarant Found!",
                     });
                 }
@@ -99,53 +102,65 @@ class CustomerMain extends Component {
         }
         const data = this.state.restaurants;
         return (
-            <div>
+            <div class="row">
                 {redirectVar}
-                <Form onSubmit={this.submitSearchEvent}>
-                    <Form.Group controlId="formBasicEventName">
-                        <Form.Label>Input Search Term</Form.Label>
-                        <Form.Control type="text" placeholder="event name" onChange={this.searchHandler} />
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlSelect1">
-                        <Form.Label>Search Category</Form.Label>
-                        <Form.Control as="select" onChange={this.categoryChangeHandler}  defaultValue="dishes">
-                            <option value="location">Location</option>
-                            <option value="cuisine">Cuisine</option>
-                            <option value="deliveryMode">Mode of Delivery</option>
-                            <option value="dishes">Dishes</option>
-                        </Form.Control>
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Search Restaurant
+                <div class="col">
+                    <Form onSubmit={this.submitSearchEvent}>
+                        <Form.Group controlId="formBasicEventName">
+                            <Form.Label>Input Search Term</Form.Label>
+                            <Form.Control type="text" placeholder="event name" onChange={this.searchHandler} />
+                        </Form.Group>
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                            <Form.Label>Search Category</Form.Label>
+                            <Form.Control as="select" onChange={this.categoryChangeHandler} defaultValue="dishes">
+                                <option value="location">Location</option>
+                                <option value="cuisine">Cuisine</option>
+                                <option value="deliveryMode">Mode of Delivery</option>
+                                <option value="dishes">Dishes</option>
+                            </Form.Control>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Search Restaurant
                     </Button>
 
-                </Form>
-                {this.state.restaurantnotfound}
+                    </Form>
+                    {this.state.restaurantnotfound}
 
-                <CardColumns>
-                    {data !== "" ? data.map((d) => {
-                        return (
-                            <Accordion>
-                                <Card>
-                                    <Card.Header>
-                                        <Card.Header as="h5"> Restaurant Name : {d.Name}</Card.Header>
-                                        <Card.Header as="h5"> Cuisine : {d.Cuisine}</Card.Header>
-                                        <Link to={{ 
-                            pathname: "/RestaurantPage", 
-                            state: d.idRestaurants, 
-                           }}>
-                                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                                            Take me to Restaurant's Page!
+                    <CardColumns>
+                        {data !== "" ? data.map((d) => {
+                            return (
+                                <Accordion>
+                                    <Card>
+                                        <Card.Header>
+                                            <Card.Header as="h5"> Restaurant Name : {d.Name}</Card.Header>
+                                            <Card.Header as="h5"> Cuisine : {d.Cuisine}</Card.Header>
+                                            <Link to={{
+                                                pathname: "/RestaurantPage",
+                                                state: d.idRestaurants,
+                                            }}>
+                                                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                                    Take me to Restaurant's Page!
                                     </Accordion.Toggle>
-                                    </Link>
-                                    </Card.Header>
-                                </Card>
-                            </Accordion>
+                                            </Link>
+                                        </Card.Header>
+                                    </Card>
+                                </Accordion>
 
-                        )
-                    }) : ""}
+                            )
+                        }) : ""}
 
-                </CardColumns>
+
+                    </CardColumns>
+                </div>
+                <div class="col-7">
+                    <div class="media">
+                        <div class="media-body">
+                            <h5 class="mt-0">Restaurants MAP</h5>
+                            <MapContainer />
+                        </div>
+                    </div>
+
+                </div>
             </div>
         );
     }
